e0b4b7df110075227254ef998b50177893d7a053
Kaj Schmidt <kaj.schmidt@ntig.se>
2024-05-15 13:19:42 +0200

    Hämtar kurser

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-﻿
+﻿class kursBlad {
+    constructor(setup) {
+        this.data = {}
+
+    }
+
+    init() {
+        this.data["courses"] = this.load()
+    }
+
+    load(setup) {
+
+        let apiUrl = 'https://api.skolverket.se/syllabus/v1/subjects?schooltype=GY&timespan=FUTURE&typeOfSyllabus=GRADE_SUBJECT_SYLLABUS';
+        let courseList = []
+
+        fetch(apiUrl)
+        .then(response => { return response.json() })
+        .then(fetchedSubjects =>{
+            for (let subject of fetchedSubjects["subjects"]) {
+                
+                fetch('https://api.skolverket.se/syllabus/v1/subjects/'+ subject["code"] +'?date=2025-07-01')
+                .then(response => { return response.json() })
+                .then(fetchedSubject => {
+                    for (let fetchedCourse of fetchedSubject["subject"]["courses"]) {
+                        let course = {
+                            "subject_name": fetchedSubject["subject"]["name"],
+                            "subject_code": fetchedSubject["subject"]["code"],
+                            "course_name": fetchedCourse["name"],
+                            "course_code": fetchedCourse["code"],
+                            "course_content": fetchedCourse["centralContent"]["text"]
+                        }
+                        courseList.push(course)
+                    }
+                })
+                
+            }
+        })
+        return courseList
+    }
+}
---
+++ todo.md
@@ -0,0 +1,7 @@
+﻿# TODO
+
+## POC
+
+[x] - API Call: kurslista
+[x] - API Call: Centralt innehåll
+[ ] - Poängplaner
